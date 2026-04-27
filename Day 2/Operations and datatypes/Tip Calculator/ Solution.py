d7106ba873be539b21b7322d7fe4bb574ecd5231
NANDINI S <nandinissomu@gmail.com>
2026-04-27 14:04:27 +0530

    Delete Day 2/Operations and datatypes/Tip Calculator directory

--- Day 2/Operations and datatypes/Tip Calculator/ Solution.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-total=(bill/people)*(1+(tip/100))
-print(f"Each person should pay: ${total:.2f}")
-
-
